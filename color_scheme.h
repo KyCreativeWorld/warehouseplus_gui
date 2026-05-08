f145c7ec4fc01fc6d6903fd0ae170be4fcdd1628
KyCreativeWold <kycreativeworld@gmail.com>
2026-05-08 13:08:29 -0700

    Added color scheme file

---
+++ color_scheme.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "raylib.h"
+
+// GLOBAL COLOR SCHEME
+// Color PRIMARY_BG = LIGHTGRAY;
+// Color SECONDARY_BG = GRAY;
+// Color ACCENT_COLOR = WHITE;
+// Color TEXT_COLOR = BLACK;
+
+inline const Color PRIMARY_BG = LIGHTGRAY;
+inline const Color SECONDARY_BG = GRAY;
+inline const Color ACCENT_COLOR = WHITE;
+inline const Color TEXT_COLOR = BLACK;
